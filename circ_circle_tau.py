d9f50466faa909b10b21a5174e448bebcba0e93d
Ubuntu <ubuntu@ip-172-31-86-29.ec2.internal>
2022-03-08 14:44:03 +0000

    Completed circumference calculator program

---
+++ circ_circle_tau.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+# Created By: Alex De Meo
+# Date: 03/08/2022
+# Description: Calculates the circumference of a circle with user input
+
+
+import math
+import constants
+
+# Calculation function that asks for input and calculates circumference
+def calculate():
+    # Loop that re asks for input if the user enters something invalid
+    while True:
+        num1 = input("Enter the radius of your circle:\n").strip()
+        # checks to make sure input is valid
+        try:
+            rad = float(num1)
+            break
+        except ValueError:
+            print(
+                "Invalid Number. Please try again with no letters or special characters."
+            )
+    # Calcuating circumference
+    circumference = constants.TAU * rad
+    # displaying the circumference
+    print("The circumference of your circle is {:.2f}cm".format(circumference))
+
+
+# main function calls calculate function
+def main():
+    calculate()
+
+
+# if statement calss main function
+if __name__ == "__main__":
+    main()
---
+++ constants.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# Created By: Alex De Meo
+# Date: 02//22
+# Description: Creates and holds the constants
+import math
+
+TAU = 2 * math.pi
